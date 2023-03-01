78015926a18e60dcbd1d251d01fb67fc2cc53c31
Wenty Li <wenty.l@nodereal.io>
2023-03-01 13:00:23 +0800

    Update

---
+++ _next/static/chunks/pages/404-fc381dc98a0be798.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{41697:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return t(86341)}])},86341:function(n,u,t){"use strict";function r(){return null}t.r(u),t.d(u,{default:function(){return r}})}},function(n){n.O(0,[774,888,179],(function(){return u=41697,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
---
+++ _next/static/nl8rHUDGFFqSU1G7VDNVC/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdocs\u002Fbasic\u002F[...slug]","\u002Fdocs\u002Fcomponents\u002F[...slug]","\u002Fdocs\u002Fhooks\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
